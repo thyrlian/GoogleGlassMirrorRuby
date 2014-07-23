0cefe01022ac43fc8cb75f90fb0ac65997b4920b
Jing Li <thyrlian@gmail.com>
2014-07-23 22:22:23 +0200

    Initial commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra/base'
+require 'sinatra/contrib'
+require 'json'
+
+require_relative 'routes/init'
+
+module GGMR
+  class App < Sinatra::Base
+    configure do
+      root_dir = File.dirname(__FILE__)
+      set :root, root_dir
+      set :app_file, File.join(root_dir, File.basename(__FILE__))
+      register Sinatra::Namespace
+      helpers Sinatra::JSON
+    end
+  end
+end
